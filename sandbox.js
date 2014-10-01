fea0fe620859669a06774d8cf90c770d45a3df9f
Adam Kucharczyk <akucharczyk@nxsystems.eu>
2014-10-01 18:09:32 +0059

    http server lights up LEDs

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,69 @@
-var LED = require('./src/singleOnBoardLED');
+var j5 = require("johnny-five");
+var http = require('http');
 
-LED.init();
+var leds = {};
 
+var myBoard = new j5.Board();
 
-//var http = require('http');
-//http.createServer(function (req, res) {
-//    res.writeHead(200, {
-//        'Content-Type': 'text/plain; charset=UTF-8'
-//    });
-//    
-//    res.end('Hello from Arduino Sandbox.\n');
-//    
-//}).listen(9080, "");
+function hide(type, leds) {
+    "use strict";
+    leds[type].off();
+}
+
+function show(type, delay, leds) {
+    "use strict";
+    try {
+        leds[type].on();
+        setTimeout(function () {
+            hide(type, leds);
+        }, delay);
+    } catch (error) {
+        console.log('no such led');
+    }
+}
+
+var initLeds = function () {
+    "use strict";
+
+    leds.success = new j5.Led(10);
+    leds.working = new j5.Led(9);
+    leds.failure = new j5.Led(8);
+    // try "on", "off", "toggle", "strobe", "stop" (stops strobing)
+
+    leds.success.off();
+    leds.working.off();
+    leds.failure.off();
+};
+
+function mainApp() {
+    "use strict";
+
+    initLeds();
+
+    http.createServer(function (req, res) {
+        var url = req.url;
+        console.log(url);
+
+        switch (url) {
+            case '/compilation/success':
+                show('success', 1000, leds);
+                break;
+            case '/compilation/working':
+                show('working', 1000, leds);
+                break;
+            case '/compilation/error':
+                show('failure', 1000, leds);
+                break;
+            default:
+                break;
+        }
+
+        // send empty response
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=UTF-8'
+        });
+        res.end('');
+    }).listen(9080, "");
+}
+
+myBoard.on("ready", mainApp);
